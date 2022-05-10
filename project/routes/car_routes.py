30f3ae39e67f31ad2dc47d831106e6705c6a211b
AidenLeeeee <hoo8799@naver.com>
2022-05-11 03:47:45 +0900

    Add bootstrap and favicons

--- project/routes/car_routes.py
+++ project/routes/car_routes.py
@@ -12,15 +12,26 @@ blueprint = Blueprint(
     url_prefix='/cars'
 )
 
+# Cars Main
 @blueprint.route('/')
 def cars():
     car_list = CarModel.query.order_by(CarModel.registered_on)
     return render_template('cars.html', car_list=car_list)
 
+# Register Car API
 @blueprint.route('/register', methods=['POST'])
 def register():
     car_name = request.form['car_name']
     car = CarModel(car_name=car_name)
     g.db.add(car)
+    g.db.commit()
+    return redirect(url_for('cars.cars'))
+
+# Delete Car API
+@blueprint.route('/delete', methods=['POST'])
+def delete():
+    car_id = request.form['car_id']
+    car = CarModel.query.get_or_404(car_id)
+    g.db.delete(car)
     g.db.commit()
     return redirect(url_for('cars.cars'))
