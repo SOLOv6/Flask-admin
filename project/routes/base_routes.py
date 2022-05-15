a82778398b314a38c20abb4a3675655d6bbd7a89
AidenLeeeee <hoo8799@naver.com>
2022-05-15 19:57:17 +0900

    Updated Event route

--- project/routes/base_routes.py
+++ project/routes/base_routes.py
@@ -1,5 +1,6 @@
 # Libraries
 from flask import Blueprint, render_template, request, g
+from sqlalchemy import and_, func
 
 # DB Models
 from project.models.event import Event as EventModel
@@ -16,6 +17,9 @@ def index():
     page = request.args.get('page',type=int ,default=1)
     event_list = EventModel.query.order_by(EventModel.created_on.desc())
     if input_from and input_to:
-        pass
+        event_list = event_list.filter(and_
+            (func.date(EventModel.created_on) >= input_from),
+            (func.date(EventModel.created_on) <= input_to)
+        )
     event_list = event_list.paginate(page, per_page=10)
     return render_template('index.html', event_list=event_list, page=page)
