544c6a0fbdc802e36b459c8e9b85950e835ae866
AidenLeeeee <hoo8799@naver.com>
2022-05-17 13:10:13 +0900

    Updated Sqlite Setting

--- project/__init__.py
+++ project/__init__.py
@@ -2,12 +2,20 @@
 from flask import Flask, g
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 # Flask configs
 import config
 
-
-db = SQLAlchemy()
+# SQLite Settings
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
@@ -22,7 +30,10 @@ def create_app():
 
     ''' DB INIT '''
     db.init_app(app)
-    migrate.init_app(app, db, render_as_batch=True)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from .models import car
     from .models import user
     from .models import event
