1dedba951ec97f22c20dc8a1d147d9659566b989
AidenLeeeee <hoo8799@naver.com>
2022-06-06 14:27:43 +0900

    Added Login and Register Routes

--- project/routes/auth_routes.py
+++ project/routes/auth_routes.py
@@ -14,14 +14,44 @@ blueprint = Blueprint(
     url_prefix='/auth'
 )
 
-@blueprint.route('/login')
+@blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    # check method 'POST' and validate is OK 
+    if form.validate_on_submit():
+        # TODO
+        # 1) 유저 조회
+        # 2) 존재하는 유저인지 확인
+        # 3) 패스워드 정합성 확인
+        # 4) 로그인 유지(Session)
+        admin_name = form.data.get('admin_name')
+        password = form.data.get('password')
+        return f'{admin_name}, {password}'
+    else:
+        # TODO: Error
+        pass
+
     return render_template('login.html', form=form)
 
-@blueprint.route('/register')
+@blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        # TODO
+        # 1) 유저 조회
+        # 2) 유저가 이미 존재하는지 확인
+        # 3) 없으면 유저 생성
+        # 4) 로그인 유지(Session)
+        admin_name = form.data.get('admin_name')
+        password = form.data.get('password')
+        repassword = form.data.get('repassword')
+        return f'{admin_name}, {password}, {repassword}'
+    else:
+        # TODO: Error
+        pass
+
     return render_template('register.html', form=form)
 
 @blueprint.route('/logout')
